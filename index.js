94937780a47f01e3cd9704d98c8e197bbe940492
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2017-12-22 13:50:49 -0800

    Removes testing code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import AppContainer from './src/components/AppContainer.react';
 
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry } from 'react-native';
 
 AppRegistry.registerComponent('Infindi', () => AppContainer);
 
@@ -12,13 +12,9 @@ AppRegistry.registerComponent('Infindi', () => AppContainer);
 //
 // -----------------------------------------------------------------------------
 
-import ModalTransition from './src/components/shared/ModalTransition.react';
-import React from 'react';
 import Store from './src/store';
 
 import { login, logout } from './src/actions/authentication';
-
-import type { ID } from 'common/src/types/core';
 
 if (__DEV__) {
   const TEST_EMAIL = 'infindi.testing@gmail.com';
@@ -30,62 +26,5 @@ if (__DEV__) {
 
   global.logout = () => {
     Store.dispatch(logout());
-  };
-
-  global.showModal = (id: ID) => {
-    const content = (
-      <View
-        style={{
-          alignItems: 'center',
-          height: 100,
-          justifyContent: 'center',
-          width: 100,
-        }}
-      >
-        <Text>{id}</Text>
-      </View>
-    );
-
-    Store.dispatch({
-      modal: {
-        id,
-        modalType: 'REACT_WITH_TRANSITION',
-        priority: 'USER_REQUESTED',
-        renderInitial: () => (
-          <ModalTransition
-            onPressBackground={() => global.hideModal(id)}
-            show={false}
-          >
-            {content}
-          </ModalTransition>
-        ),
-        renderIn: () => (
-          <ModalTransition
-            onPressBackground={() => global.hideModal(id)}
-            show={true}
-          >
-            {content}
-          </ModalTransition>
-        ),
-        renderTransitionOut: () => (
-          <ModalTransition
-            onPressBackground={() => global.hideModal(id)}
-            show={false}
-          >
-            {content}
-          </ModalTransition>
-        ),
-        transitionInMillis: 400,
-        transitionOutMillis: 400,
-      },
-      type: 'REQUEST_MODAL',
-    });
-  };
-
-  global.hideModal = (id: ID) => {
-    Store.dispatch({
-      modalID: id,
-      type: 'DISMISS_MODAL',
-    });
   };
 }
