3d010100e23a44bea795b6084ed33088cbcbbd3a
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-01-27 13:00:20 -0800

    Adds username / password page

--- src/yodlee/AccountLogin.react.js
+++ src/yodlee/AccountLogin.react.js
@@ -3,8 +3,19 @@
 import Colors from '../design/colors';
 import Icons from '../design/icons';
 import React, { Component } from 'react';
+import TextButton from '../components/shared/TextButton.react';
+import TextDesign from '../design/text';
 
-import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import invariant from 'invariant';
+
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { NavBarHeight } from '../design/layout';
 
 import type { Provider as YodleeProvider } from 'common/lib/models/YodleeProvider';
@@ -14,8 +25,21 @@ export type Props = {
   provider: YodleeProvider,
 };
 
-export default class AccountLogin extends Component<Props> {
+type State = {
+  password: string,
+  userName: string,
+};
+
+const LEFT_ARROW_WIDTH = 18;
+
+export default class AccountLogin extends Component<Props, State> {
+  state: State = {
+    password: '',
+    userName: '',
+  };
+
   render() {
+    const { raw: rawProvider } = this.props.provider;
     return (
       <View style={styles.root}>
         <View style={styles.header}>
@@ -26,10 +50,53 @@ export default class AccountLogin extends Component<Props> {
               style={styles.headerLeftIcon}
             />
           </TouchableOpacity>
+          <Text style={[TextDesign.header3, styles.headerTitle]}>
+            {rawProvider.name}
+          </Text>
+          <View style={styles.headerRightIcon} />
+        </View>
+        <View style={styles.content}>
+          <TextInput
+            autoFocus={true}
+            onChangeText={this._onChangeUserName}
+            placeholder="Username"
+            style={styles.userNameInput}
+            value={this.state.userName}
+          />
+          <TextInput
+            onChangeText={this._onChangePassword}
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.passwordInput}
+            value={this.state.password}
+          />
+          {Boolean(rawProvider.forgetPasswordUrl) && (
+            <TextButton
+              onPress={this._onPressForgotCredentials}
+              size="SMALL"
+              type="SPECIAL"
+              text="Forgot username or password?"
+            />
+          )}
         </View>
       </View>
     );
   }
+
+  _onChangeUserName = (text: string): void => {
+    this.setState({ userName: text });
+  };
+
+  _onChangePassword = (text: string): void => {
+    this.setState({ password: text });
+  };
+
+  _onPressForgotCredentials = (): void => {
+    const { forgetPasswordUrl } = this.props.provider.raw;
+    invariant(forgetPasswordUrl, 'Cannot select forget password without url');
+    // TODO: Open safari at the url.
+    // https://facebook.github.io/react-native/docs/linking.html
+  };
 
   _onPressHeaderLeftIcon = (): void => {
     this.props.onBack();
@@ -37,6 +104,20 @@ export default class AccountLogin extends Component<Props> {
 }
 
 const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    marginTop: 32,
+  },
+
+  userNameInput: {
+    borderBottomWidth: 1,
+    borderColor: Colors.BORDER,
+    fontFamily: TextDesign.thickFont,
+    fontSize: TextDesign.largeFontSize,
+    marginHorizontal: 24,
+    paddingBottom: 4,
+  },
+
   header: {
     alignItems: 'center',
     borderBottomWidth: 1,
@@ -47,6 +128,26 @@ const styles = StyleSheet.create({
 
   headerLeftIcon: {
     marginLeft: 16,
+    width: LEFT_ARROW_WIDTH,
+  },
+
+  headerTitle: {
+    flex: 1,
+    textAlign: 'center',
+  },
+
+  headerRightIcon: {
+    marginRight: 16,
+    width: LEFT_ARROW_WIDTH,
+  },
+
+  passwordInput: {
+    borderBottomWidth: 1,
+    borderColor: Colors.BORDER,
+    fontFamily: TextDesign.thickFont,
+    fontSize: TextDesign.largeFontSize,
+    margin: 24,
+    paddingBottom: 4,
   },
 
   root: {
