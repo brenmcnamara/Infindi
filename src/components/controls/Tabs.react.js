f0e116b5ea67537de625657fa06de9647ea45c20
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-03-17 14:01:59 -0700

    Removes navigator abstraction

--- src/components/controls/Tabs.react.js
+++ src/components/controls/Tabs.react.js
@@ -1,18 +1,24 @@
 /* @flow */
 
 import AccountsScreen from '../AccountsScreen.react';
-import Navigator from './Navigator.react';
+import Colors from '../../design/colors';
+import Icons from '../../design/icons';
 import React, { Component } from 'react';
 
 import invariant from 'invariant';
 
+import { connect } from 'react-redux';
+import { NavigatorIOS } from 'react-native';
+import { requestLeftPane } from '../../actions/modal';
+
+import type { ReduxProps } from '../../typesDEPRECATED/redux';
 import type { RouteNode } from '../../common/route-utils';
 
-export type Props = {
+export type Props = ReduxProps & {
   routeNode: RouteNode,
 };
 
-export default class Tabs extends Component<Props> {
+class Tabs extends Component<Props> {
   componentDidMount(): void {
     invariant(
       this.props.routeNode.name === 'ACCOUNTS',
@@ -28,10 +34,27 @@ export default class Tabs extends Component<Props> {
   }
 
   render() {
+    const Component = AccountsScreen;
+    const couldBeScrollable = this.props.routeNode.name === 'ACCOUNTS';
     return (
-      <Navigator
-        payload={{ component: AccountsScreen, couldBeScrollable: true }}
+      <NavigatorIOS
+        initialRoute={{
+          barTintColor: Colors.BACKGROUND,
+          component: Component,
+          leftButtonIcon: Icons.List,
+          onLeftButtonPress: this._onPressLeftButton,
+          shadowHidden: !couldBeScrollable,
+          tintColor: Colors.NAV_BAR_BUTTON,
+          title: '',
+        }}
+        style={{ flex: 1 }}
       />
     );
   }
+
+  _onPressLeftButton = (): void => {
+    this.props.dispatch(requestLeftPane());
+  };
 }
+
+export default connect()(Tabs);
