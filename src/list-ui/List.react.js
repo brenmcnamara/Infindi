08a8e4e068ec0dee538ee04dbfb54a6fed1581b5
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-04-29 22:54:35 -0700

    Removes all animations

--- src/list-ui/List.react.js
+++ src/list-ui/List.react.js
@@ -3,9 +3,7 @@
 import ListItem from './ListItem.react';
 import React, { Component } from 'react';
 
-import invariant from 'invariant';
-
-import { Animated, Easing, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 
 import type { ComponentType } from 'react';
 
@@ -29,53 +27,16 @@ export type Props = {
   keyboardShouldPersistTaps?: 'always',
 };
 
-export type ListItemChange =
-  | {|
-      +type: 'INITIALIZE',
-    |}
-  | {|
-      +type: 'ADD',
-    |}
-  | {|
-      +type: 'REMOVE',
-    |}
-  | {|
-      type: 'NONE',
-    |};
-
-const ITEM_CHANGE_INITIALIZE = { type: 'INITIALIZE' };
-const ITEM_CHANGE_ADD = { type: 'ADD' };
-const ITEM_CHANGE_REMOVE = { type: 'REMOVE' };
-const ITEM_CHANGE_NONE = { type: 'NONE' };
-
-const TRANSITION_INITIALIZE_DURATION_PER_ITEM_MS = 200;
-const TRANSITION_INITIALIZE_DELAY_PER_ITEM_MS = 30;
-const TRANSITION_INITIALIZE_EASING = Easing.out(Easing.poly(5));
-
 type State = {
   initializeCount: number,
   itemOrdering: Array<string>,
   itemMap: { [key: string]: ListItemData },
-  itemChangeMap: { [key: string]: ListItemChange },
-  isListInitializing: boolean,
 };
 
 export default class List extends Component<Props, State> {
-  _initializeTransition: Animated.Value = new Animated.Value(0);
-
   constructor(props: Props) {
     super(props);
     this.state = this._calculateState(props, /* isInitializing */ true);
-  }
-
-  componentDidMount(): void {
-    Animated.timing(this._initializeTransition, {
-      duration:
-        TRANSITION_INITIALIZE_DELAY_PER_ITEM_MS * this.state.initializeCount,
-      easing: TRANSITION_INITIALIZE_EASING,
-      toValue: 1,
-      useNativeDriver: true,
-    }).start();
   }
 
   componentWillReceiveProps(nextProps: Props): void {
@@ -106,28 +67,10 @@ export default class List extends Component<Props, State> {
 
   _renderRowItem = ({ index, item: itemKey }) => {
     const item = this.state.itemMap[itemKey];
-    const change = this.state.itemChangeMap[itemKey];
-    const totalDuration =
-      (this.state.initializeCount - 1) *
-        TRANSITION_INITIALIZE_DELAY_PER_ITEM_MS +
-      TRANSITION_INITIALIZE_DURATION_PER_ITEM_MS;
-    const initializeStart =
-      index * TRANSITION_INITIALIZE_DELAY_PER_ITEM_MS / totalDuration;
-
     const { Comp, height } = item;
     return (
-      <ListItem
-        height={height}
-        index={index}
-        initializeStart={initializeStart}
-        initializeTransition={this._initializeTransition}
-        isListInitializing={this.state.isListInitializing}
-        onAddTransitionComplete={() => this._onAddTransitionComplete(itemKey)}
-        onRemoveTransitionComplete={() =>
-          this._onRemoveTransitionComplete(itemKey)
-        }
-      >
-        {change.type === 'REMOVE' ? null : <Comp />}
+      <ListItem height={height} index={index}>
+        <Comp />
       </ListItem>
     );
   };
@@ -139,68 +82,15 @@ export default class List extends Component<Props, State> {
     const { data } = props;
     const itemOrdering = data.map(item => item.key);
     const itemMap = {};
-    const itemChangeMap = {};
     data.forEach(item => {
-      let change;
-      if (isInitializing) {
-        change = ITEM_CHANGE_INITIALIZE;
-      } else if (!this.state.itemMap[item.key]) {
-        change = ITEM_CHANGE_ADD;
-      } else {
-        change = ITEM_CHANGE_NONE;
-      }
       itemMap[item.key] = item;
-      itemChangeMap[item.key] = change;
-    });
-
-    if (isInitializing) {
-      return {
-        initializeCount: itemOrdering.length,
-        isListInitializing: true,
-        itemChangeMap,
-        itemMap,
-        itemOrdering,
-      };
-    }
-
-    // Check for any items that were removed. This should only happen if we
-    // are not initializing.
-    this.state.itemOrdering.forEach((itemKey, index) => {
-      if (!itemMap[itemKey]) {
-        itemOrdering.splice(index, 0, itemKey);
-        itemChangeMap[itemKey] = ITEM_CHANGE_REMOVE;
-        itemMap[itemKey] = this.state.itemMap[itemKey];
-      }
     });
 
     return {
-      isListInitializing: false,
-      itemChangeMap,
       itemMap,
       itemOrdering,
     };
   }
 
   _keyExtractor = (key: string) => key;
-
-  _onAddTransitionComplete = (itemKey: string): void => {};
-
-  _onRemoveTransitionComplete = (itemKey: string): void => {
-    const itemOrdering = this.state.itemOrdering.slice();
-    const index = itemOrdering.indexOf(itemKey);
-    invariant(
-      index >= 0,
-      'Trying to remove item that deos not exist: %s',
-      itemKey,
-    );
-    itemOrdering.splice(index, 1);
-
-    const itemChangeMap = { ...this.state.itemChangeMap };
-    delete itemChangeMap[itemKey];
-
-    const itemMap = { ...this.state.itemMap };
-    delete itemMap[itemKey];
-
-    this.setState({ itemChangeMap, itemMap, itemOrdering });
-  };
 }
--- src/list-ui/ListItem.react.js
+++ src/list-ui/ListItem.react.js
@@ -2,95 +2,19 @@
 
 import React, { Component } from 'react';
 
-import { Animated, Easing } from 'react-native';
+import { View } from 'react-native';
 
 export type Props = {
   children?: ?any,
   height: number,
   index: number,
-  initializeStart: number,
-  initializeTransition: Animated.Value,
-  isListInitializing: boolean,
-  onAddTransitionComplete: () => void,
-  onRemoveTransitionComplete: () => void,
 };
 
-const TRANSITION_INSERT_OR_REMOVE_DURATION_MS = 500;
-const TRANSITION_INSERT_OR_REMOVE_EASING = Easing.out(Easing.cubic);
 
 export default class ListItem extends Component<Props> {
-  _addOrRemoveTransition = new Animated.Value(0);
-
-  componentWillMount(): void {
-    if (this.props.isListInitializing) {
-      this._addOrRemoveTransition.setValue(1);
-    }
-  }
-
-  componentDidMount(): void {
-    if (!this.props.isListInitializing) {
-      // TODO: Not sure why I need to defer the animation with this timeout,
-      // but I should probably submit an issue to react native.
-      setTimeout(() => {
-        Animated.timing(this._addOrRemoveTransition, {
-          duration: TRANSITION_INSERT_OR_REMOVE_DURATION_MS,
-          easing: TRANSITION_INSERT_OR_REMOVE_EASING,
-          toValue: 1,
-        }).start(this.props.onAddTransitionComplete);
-      }, 0);
-    }
-  }
-
-  componentWillReceiveProps(nextProps: Props): void {
-    if (React.Children.count(nextProps.children) === 0) {
-      Animated.timing(this._addOrRemoveTransition, {
-        duration: TRANSITION_INSERT_OR_REMOVE_DURATION_MS,
-        easing: TRANSITION_INSERT_OR_REMOVE_EASING,
-        toValue: 0,
-      }).start(nextProps.onRemoveTransitionComplete);
-    }
-  }
-
   render() {
-    const { initializeStart, initializeTransition } = this.props;
-    const outerStyles = {
-      opacity: initializeTransition.interpolate({
-        inputRange: [0, Math.min(1, initializeStart), 1],
-        outputRange: [0, 0, 1],
-      }),
-      transform: [
-        {
-          translateY: initializeTransition.interpolate({
-            inputRange: [0, Math.min(1, initializeStart), 1],
-            outputRange: [-20, -20, 0],
-          }),
-        },
-      ],
-    };
-
-    const innerStyles = {
-      height: this._addOrRemoveTransition.interpolate({
-        inputRange: [0, 1],
-        outputRange: [0, this.props.height],
-      }),
-      opacity: this._addOrRemoveTransition.interpolate({
-        inputRange: [0, 0.3, 1],
-        outputRange: [0, 0, 1],
-      }),
-      transform: [
-        {
-          translateX: this._addOrRemoveTransition.interpolate({
-            inputRange: [0, 1],
-            outputRange: [-40, 0],
-          }),
-        },
-      ],
-    };
-
     return (
-      <Animated.View style={outerStyles}>
-        <Animated.View style={innerStyles}>{this.props.children}</Animated.View>
-      </Animated.View>
+      <View style={{ height: this.props.height }}>{this.props.children}</View>
     );
   }
 }
