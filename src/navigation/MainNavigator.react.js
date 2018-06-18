9e376ee422c478f876d1f36ac2c1ab91b757b171
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-06-18 09:03:19 -0700

    Adds navigation to and from transactions

--- src/navigation/MainNavigator.react.js
+++ src/navigation/MainNavigator.react.js
@@ -1,10 +1,15 @@
 /* @flow */
 
 import * as React from 'react';
+import AccountDetailsScreen from '../components/AccountDetailsScreen.react';
 import AccountsScreen from '../components/AccountsScreen.react';
 import StackNavigator from './StackNavigator.react';
 
-import type { ReduxState } from '../store';
+import invariant from 'invariant';
+
+import { exitAccountDetails } from '../actions/router';
+
+import type { Action, ReduxState } from '../store';
 
 export type Props = {};
 
@@ -12,6 +17,7 @@ export default class MainNavigator extends React.Component<Props> {
   render() {
     return (
       <StackNavigator
+        calculateBackAction={this._calculateBackAction}
         calculateStackForState={this._calculateStackForState}
         isBarShadowShowing={true}
         screens={Screens}
@@ -19,12 +25,32 @@ export default class MainNavigator extends React.Component<Props> {
     );
   }
 
+  _calculateBackAction = (
+    prevScreen: string,
+    currentScreen: string,
+  ): Action => {
+    const change = `${currentScreen} -> ${prevScreen}`;
+    switch (change) {
+      case 'AccountDetails -> Accounts':
+        return exitAccountDetails();
+
+      default:
+        return invariant(false, `Unrecognized screen change: ${change}`);
+    }
+  };
+
   _calculateStackForState = (state: ReduxState): Array<string> => {
-    return ['Accounts'];
+    return state.routeState.accountDetailsID
+      ? ['Accounts', 'AccountDetails']
+      : ['Accounts'];
   };
 }
 
 const Screens = [
+  {
+    component: AccountDetailsScreen,
+    screen: 'AccountDetails',
+  },
   {
     component: AccountsScreen,
     screen: 'Accounts',
