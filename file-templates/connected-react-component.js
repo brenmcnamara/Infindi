341e0b91abb9605bb689bda0aa8b41368c69eeab
Brendan McNamara <2343120+brenmcnamara@users.noreply.github.com>
2018-06-25 16:12:12 -0700

    Create connected-react-component.js

---
+++ file-templates/connected-react-component.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+import * as React from 'react';
+
+import {connect} from 'react-redux';
+import {StyleSheet} from 'react-native';
+
+import {ReduxProps, ReduxState} from './store'; // TODO: Fix path
+
+export type Props = ReduxProps & ComponentProps & ComputedProps;
+
+type ComponentProps = {}; // TODO: Create type
+
+type ComputedProps = {}; // TODO: Create type
+
+type State = {};
+
+/**
+ * TODO: Fill in the document here.
+ * TODO: Rename the component
+ */
+class Component extends React.Component<Props, State> {
+  state: State = {};
+
+  render() {
+    // TODO: Implement me
+  }
+}
+
+
+function mapReduxStateToProps(reduxState: ReduxState): ComputedProps {
+  return {}; // TODO: Implement me
+}
+
+// TODO: Styles
+const styles = StyleSheet.create({});
+
+export default connect(mapReduxStateToProps)(Component); // TODO: Rename component
